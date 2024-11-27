f48ae9d35937ec004c7014086f7a7dde17e2fdba
Oroprogamer23 <pangilinan.dericknathaniel@auf.edu.ph>
2024-11-27 13:11:45 +0800

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace Module08
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void ClickedViewUser(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//UserPage"); 
+        }
+    }
+
+}
